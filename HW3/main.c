0d945f79587c33b52e588162bf903f8670a5176e
Jonathan Gescheit <jonathan.ges@gmail.com>
2017-06-15 20:25:33 +0300

    Kawabanga

--- HW3/main.c
+++ HW3/main.c
@@ -12,13 +12,7 @@ int main() {
     SPCommand command;
     SPFiarGame* game;
 
-    printf("Please enter the difficulty level between [1-7]:\n");
-    gets(userInput);
-
-    if (spParserIsInt(userInput)) {
-        level = strtoumax(userInput, NULL, 10);
-        if (level < 1 || level > 7) printf("error");
-    }
+    level = getLevel();
 
     game = spFiarGameCreate(HISTORY_SIZE);
     spFiarGamePrintBoard(game);
@@ -28,18 +22,42 @@ int main() {
         gets(userInput);
         command = spParserPraseLine(userInput);
 
-        spFiarGameSetMove(game, command.arg - 1);
+        if (command.cmd == SP_SUGGEST_MOVE) {
+            int suggestedMove = spMinimaxSuggestMove(game, level);
+            printf("Suggested move: drop a disc to column %d\n", suggestedMove);
+        } else if (command.cmd == SP_UNDO_MOVE) {
+            spFiarGameUndoPrevMove(game);
+            continue;
+        } else if (command.cmd == SP_ADD_DISC && command.validArg) {
+            if (command.arg < 1 || command.arg > 7) {
+                printf("Error: column number must be in range 1-7\n");
+                continue;
+            }
 
-        winner = spFiarCheckWinner(game);
+            if (!spFiarGameIsValidMove(game, command.arg - 1)) {
+                printf("Error: column %d is full\n", command.arg);
+                continue;
+            }
 
-        if (winner == '\0') {
-            int computerMove = spMinimaxSuggestMove(game, level);
-            spFiarGameSetMove(game, computerMove);
+            spFiarGameSetMove(game, command.arg - 1);
+
+            winner = spFiarCheckWinner(game);
+
+            if (winner == '\0') {
+                int computerMove = spMinimaxSuggestMove(game, level);
+                spFiarGameSetMove(game, computerMove);
+            }
+
+            spFiarGamePrintBoard(game);
+
+            winner = spFiarCheckWinner(game);
+        } else if (command.cmd == SP_RESTART) {
+            printf("restart");
+        } else if (command.cmd == SP_QUIT) {
+            printf("quit");
+        } else if (command.cmd == SP_INVALID_LINE) {
+            printf("invalid command");
         }
-
-        spFiarGamePrintBoard(game);
-
-        winner = spFiarCheckWinner(game);
     }
 
     printf("%c", winner);
@@ -47,4 +65,22 @@ int main() {
 
 
     return 0;
+}
+
+int getLevel() {
+    char userInput[1024];
+    int level;
+
+    printf("Please enter the difficulty level between [1-7]:\n");
+    gets(userInput);
+
+    if (spParserIsInt(userInput)) {
+        level = strtoumax(userInput, NULL, 10);
+        if (level < 1 || level > 7) {
+            printf("Error: invalid level (should be between 1 to 7\n");
+            getLevel();
+        }
+    }
+
+    return level;
 }
